acf6458f01631e46c012b79f1a447ebdae92b8c0
insertGG <105223721+insertGG@users.noreply.github.com>
2023-11-22 00:30:18 -0300

    Avanzo en el apartado front-end
    hice todas las partes principales de la página a groso modo, pendiente por refinar

--- expand.js
+++ expand.js
@@ -7,5 +7,7 @@ function toggleText() {
   hideText.classList.toggle("show");
   if (hideText.classList.contains("show")) {
     hideText_btn.innerHTML = "Ver menos";
-  } else hideText_btn.innerHTML = "Ver más";
+  } else {
+    hideText_btn.innerHTML = "Ver más";
+  }
 }
